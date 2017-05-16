d6d48d908f422c2b61d19762b4a43547707e2963
spgitmonish <monish.sunku@walletbuddies.co>
2017-05-15 20:34:40 -0600

    Code structure cleanup

--- CarND-Kidnapped-Vehicle-Project/src/helper_functions.h
+++ CarND-Kidnapped-Vehicle-Project/src/helper_functions.h
@@ -14,76 +14,76 @@
 #include <vector>
 #include "map.h"
 
-/*
- * Struct representing one position/control measurement.
- */
-struct control_s {
-	
+// Struct representing one position/control measurement.
+struct control_s
+{
 	double velocity;	// Velocity [m/s]
 	double yawrate;		// Yaw rate [rad/s]
 };
 
-/*
- * Struct representing one ground truth position.
- */
+// Struct representing one ground truth position.
 struct ground_truth {
-	
-	double x;		// Global vehicle x position [m]
-	double y;		// Global vehicle y position
+	double x;			// Global vehicle x position [m]
+	double y;			// Global vehicle y position
 	double theta;	// Global vehicle yaw [rad]
 };
 
-/*
- * Struct representing one landmark observation measurement.
- */
+// Struct representing one landmark observation measurement.
 struct LandmarkObs {
-	
-	int id;				// Id of matching landmark in the map.
-	double x;			// Local (vehicle coordinates) x position of landmark observation [m]
-	double y;			// Local (vehicle coordinates) y position of landmark observation [m]
+	int id;			// Id of matching landmark in the map.
+	double x;		// Local (vehicle coordinates) x position of landmark observation [m]
+	double y;		// Local (vehicle coordinates) y position of landmark observation [m]
 };
 
 /*
  * Computes the Euclidean distance between two 2D points.
- * @param (x1,y1) x and y coordinates of first point
- * @param (x2,y2) x and y coordinates of second point
+ * @param (x1, y1) x and y coordinates of first point
+ * @param (x2, y2) x and y coordinates of second point
  * @output Euclidean distance between two 2D points
  */
-inline double dist(double x1, double y1, double x2, double y2) {
+inline double dist(double x1, double y1, double x2, double y2)
+{
 	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
-inline double * getError(double gt_x, double gt_y, double gt_theta, double pf_x, double pf_y, double pf_theta) {
+// Return the error for each of the parameters using the ground truth
+inline double * getError(double gt_x, double gt_y, double gt_theta, double pf_x, double pf_y, double pf_theta)
+{
 	static double error[3];
 	error[0] = fabs(pf_x - gt_x);
 	error[1] = fabs(pf_y - gt_y);
 	error[2] = fabs(pf_theta - gt_theta);
 	error[2] = fmod(error[2], 2.0 * M_PI);
-	if (error[2] > M_PI) {
+
+	if (error[2] > M_PI)
+	{
 		error[2] = 2.0 * M_PI - error[2];
 	}
+
 	return error;
 }
 
 /* Reads map data from a file.
- * @param filename Name of file containing map data.
+ * @param filename: Name of file containing map data.
  * @output True if opening and reading file was successful
  */
-inline bool read_map_data(std::string filename, Map& map) {
-
+inline bool read_map_data(std::string filename, Map& map)
+{
 	// Get file of map:
 	std::ifstream in_file_map(filename.c_str(),std::ifstream::in);
+
 	// Return if we can't open the file.
-	if (!in_file_map) {
+	if (!in_file_map)
+	{
 		return false;
 	}
-	
+
 	// Declare single line of map file:
 	std::string line_map;
 
 	// Run over each single line:
-	while(getline(in_file_map, line_map)){
-
+	while(getline(in_file_map, line_map))
+	{
 		std::istringstream iss_map(line_map);
 
 		// Declare landmark values and ID:
@@ -106,19 +106,22 @@ inline bool read_map_data(std::string filename, Map& map) {
 		// Add to landmark list of map:
 		map.landmark_list.push_back(single_landmark_temp);
 	}
+
 	return true;
 }
 
 /* Reads control data from a file.
- * @param filename Name of file containing control measurements.
+ * @param filename: Name of file containing control measurements.
  * @output True if opening and reading file was successful
  */
-inline bool read_control_data(std::string filename, std::vector<control_s>& position_meas) {
-
+inline bool read_control_data(std::string filename, std::vector<control_s>& position_meas)
+{
 	// Get file of position measurements:
 	std::ifstream in_file_pos(filename.c_str(),std::ifstream::in);
+
 	// Return if we can't open the file.
-	if (!in_file_pos) {
+	if (!in_file_pos)
+	{
 		return false;
 	}
 
@@ -126,8 +129,8 @@ inline bool read_control_data(std::string filename, std::vector<control_s>& posi
 	std::string line_pos;
 
 	// Run over each single line:
-	while(getline(in_file_pos, line_pos)){
-
+	while(getline(in_file_pos, line_pos))
+	{
 		std::istringstream iss_pos(line_pos);
 
 		// Declare position values:
@@ -136,12 +139,10 @@ inline bool read_control_data(std::string filename, std::vector<control_s>& posi
 		// Declare single control measurement:
 		control_s meas;
 
-		//read data from line to values:
-
+		// Read data from line to values:
 		iss_pos >> velocity;
 		iss_pos >> yawrate;
 
-		
 		// Set values
 		meas.velocity = velocity;
 		meas.yawrate = yawrate;
@@ -149,19 +150,22 @@ inline bool read_control_data(std::string filename, std::vector<control_s>& posi
 		// Add to list of control measurements:
 		position_meas.push_back(meas);
 	}
+
 	return true;
 }
 
 /* Reads ground truth data from a file.
- * @param filename Name of file containing ground truth.
+ * @param filename: Name of file containing ground truth.
  * @output True if opening and reading file was successful
  */
-inline bool read_gt_data(std::string filename, std::vector<ground_truth>& gt) {
-
+inline bool read_gt_data(std::string filename, std::vector<ground_truth>& gt)
+{
 	// Get file of position measurements:
 	std::ifstream in_file_pos(filename.c_str(),std::ifstream::in);
+
 	// Return if we can't open the file.
-	if (!in_file_pos) {
+	if (!in_file_pos)
+	{
 		return false;
 	}
 
@@ -169,17 +173,17 @@ inline bool read_gt_data(std::string filename, std::vector<ground_truth>& gt) {
 	std::string line_pos;
 
 	// Run over each single line:
-	while(getline(in_file_pos, line_pos)){
-
+	while(getline(in_file_pos, line_pos))
+	{
 		std::istringstream iss_pos(line_pos);
 
 		// Declare position values:
 		double x, y, azimuth;
 
 		// Declare single ground truth:
-		ground_truth single_gt; 
+		ground_truth single_gt;
 
-		//read data from line to values:
+		// Read data from line to values:
 		iss_pos >> x;
 		iss_pos >> y;
 		iss_pos >> azimuth;
@@ -192,19 +196,22 @@ inline bool read_gt_data(std::string filename, std::vector<ground_truth>& gt) {
 		// Add to list of control measurements and ground truth:
 		gt.push_back(single_gt);
 	}
+
 	return true;
 }
 
 /* Reads landmark observation data from a file.
- * @param filename Name of file containing landmark observation measurements.
+ * @param filename: Name of file containing landmark observation measurements.
  * @output True if opening and reading file was successful
  */
-inline bool read_landmark_data(std::string filename, std::vector<LandmarkObs>& observations) {
-
+inline bool read_landmark_data(std::string filename, std::vector<LandmarkObs>& observations)
+{
 	// Get file of landmark measurements:
 	std::ifstream in_file_obs(filename.c_str(),std::ifstream::in);
+
 	// Return if we can't open the file.
-	if (!in_file_obs) {
+	if (!in_file_obs)
+	{
 		return false;
 	}
 
@@ -212,14 +219,14 @@ inline bool read_landmark_data(std::string filename, std::vector<LandmarkObs>& o
 	std::string line_obs;
 
 	// Run over each single line:
-	while(getline(in_file_obs, line_obs)){
-
+	while(getline(in_file_obs, line_obs))
+	{
 		std::istringstream iss_obs(line_obs);
 
 		// Declare position values:
 		double local_x, local_y;
 
-		//read data from line to values:
+		// Read data from line to values:
 		iss_obs >> local_x;
 		iss_obs >> local_y;
 
@@ -233,6 +240,7 @@ inline bool read_landmark_data(std::string filename, std::vector<LandmarkObs>& o
 		// Add to list of control measurements:
 		observations.push_back(meas);
 	}
+
 	return true;
 }
 
--- CarND-Kidnapped-Vehicle-Project/src/main.cpp
+++ CarND-Kidnapped-Vehicle-Project/src/main.cpp
@@ -1,10 +1,3 @@
-/*
- * main.cpp
- * Reads in data and runs 2D particle filter.
- *  Created on: Dec 13, 2016
- *      Author: Tiffany Huang
- */
-
 #include <iostream>
 #include <ctime>
 #include <iomanip>
@@ -15,34 +8,37 @@
 
 using namespace std;
 
-
-
-int main() {
-
-	// parameters related to grading.
-	int time_steps_before_lock_required = 100; // number of time steps before accuracy is checked by grader.
-	double max_runtime = 45; // Max allowable runtime to pass [sec]
-	double max_translation_error = 1; // Max allowable translation error to pass [m]
-	double max_yaw_error = 0.05; // Max allowable yaw error [rad]
-
-
+int main()
+{
+	// NOTE: These parameters are related to grading.
+	// Number of time steps before accuracy is checked by grader.
+	int time_steps_before_lock_required = 100;
+	// Max allowable runtime to pass [sec]
+	double max_runtime = 45;
+	// Max allowable translation error to pass [m]
+	double max_translation_error = 1;
+	// Max allowable yaw error [rad]
+	double max_yaw_error = 0.05;
 
 	// Start timer.
 	int start = clock();
 
-	//Set up parameters here
-	double delta_t = 0.1; // Time elapsed between measurements [sec]
-	double sensor_range = 50; // Sensor range [m]
+	// Time elapsed between measurements [sec]
+	double delta_t = 0.1;
+	// Sensor range [m]
+	double sensor_range = 50;
 
 	/*
 	 * Sigmas - just an estimate, usually comes from uncertainty of sensor, but
 	 * if you used fused data from multiple sensors, it's difficult to find
 	 * these uncertainties directly.
 	 */
-	double sigma_pos [3] = {0.3, 0.3, 0.01}; // GPS measurement uncertainty [x [m], y [m], theta [rad]]
-	double sigma_landmark [2] = {0.3, 0.3}; // Landmark measurement uncertainty [x [m], y [m]]
+	// GPS measurement uncertainty [x [m], y [m], theta [rad]]
+	double sigma_pos [3] = {0.3, 0.3, 0.01};
+	// Landmark measurement uncertainty [x [m], y [m]]
+	double sigma_landmark [2] = {0.3, 0.3};
 
-	// Noise generation
+	// Noise generation(normal distribution)
 	default_random_engine gen;
 	normal_distribution<double> N_x_init(0, sigma_pos[0]);
 	normal_distribution<double> N_y_init(0, sigma_pos[1]);
@@ -53,57 +49,68 @@ int main() {
 
 	// Read map data
 	Map map;
-	if (!read_map_data("data/map_data.txt", map)) {
+	if (!read_map_data("data/map_data.txt", map))
+	{
 		cout << "Error: Could not open map file" << endl;
 		return -1;
 	}
 
 	// Read position data
 	vector<control_s> position_meas;
-	if (!read_control_data("data/control_data.txt", position_meas)) {
+	if (!read_control_data("data/control_data.txt", position_meas))
+	{
 		cout << "Error: Could not open position/control measurement file" << endl;
 		return -1;
 	}
 
 	// Read ground truth data
 	vector<ground_truth> gt;
-	if (!read_gt_data("data/gt_data.txt", gt)) {
+	if (!read_gt_data("data/gt_data.txt", gt))
+	{
 		cout << "Error: Could not open ground truth data file" << endl;
 		return -1;
 	}
 
 	// Run particle filter!
 	int num_time_steps = position_meas.size();
+	// Object of particle filter class
 	ParticleFilter pf;
-	double total_error[3] = {0,0,0};
-	double cum_mean_error[3] = {0,0,0};
+	// Variables to keep track of the error
+	double total_error[3] = {0, 0, 0};
+	double cum_mean_error[3] = {0, 0, 0};
 
-	for (int i = 0; i < num_time_steps; ++i) {
+	for (int i = 0; i < num_time_steps; ++i)
+	{
 		cout << "Time step: " << i << endl;
 		// Read in landmark observations for current time step.
 		ostringstream file;
 		file << "data/observation/observations_" << setfill('0') << setw(6) << i+1 << ".txt";
 		vector<LandmarkObs> observations;
-		if (!read_landmark_data(file.str(), observations)) {
+		if (!read_landmark_data(file.str(), observations))
+		{
 			cout << "Error: Could not open observation file " << i+1 << endl;
 			return -1;
 		}
 
 		// Initialize particle filter if this is the first time step.
-		if (!pf.initialized()) {
+		if (!pf.initialized())
+		{
 			n_x = N_x_init(gen);
 			n_y = N_y_init(gen);
 			n_theta = N_theta_init(gen);
 			pf.init(gt[i].x + n_x, gt[i].y + n_y, gt[i].theta + n_theta, sigma_pos);
 		}
-		else {
+		else
+		{
 			// Predict the vehicle's next state (noiseless).
 			pf.prediction(delta_t, sigma_pos, position_meas[i-1].velocity, position_meas[i-1].yawrate);
 		}
-		// simulate the addition of noise to noiseless observation data.
+
+		// Simulate the addition of noise to noiseless observation data.
 		vector<LandmarkObs> noisy_observations;
 		LandmarkObs obs;
-		for (int j = 0; j < observations.size(); ++j) {
+		for (int j = 0; j < observations.size(); ++j)
+		{
 			n_x = N_obs_x(gen);
 			n_y = N_obs_y(gen);
 			obs = observations[j];
@@ -112,24 +119,29 @@ int main() {
 			noisy_observations.push_back(obs);
 		}
 
-		// Update the weights and resample
+		// Update the weights of the particles and resample
 		pf.updateWeights(sensor_range, sigma_landmark, noisy_observations, map);
 		pf.resample();
 
-		// Calculate and output the average weighted error of the particle filter over all time steps so far.
+		// Calculate and output the average weighted error of the particle filter
+		// over all time steps so far.
 		vector<Particle> particles = pf.particles;
 		int num_particles = particles.size();
 		double highest_weight = 0.0;
 		Particle best_particle;
-		for (int i = 0; i < num_particles; ++i) {
-			if (particles[i].weight > highest_weight) {
+
+		for (int i = 0; i < num_particles; ++i)
+		{
+			if (particles[i].weight > highest_weight)
+			{
 				highest_weight = particles[i].weight;
 				best_particle = particles[i];
 			}
 		}
 		double *avg_error = getError(gt[i].x, gt[i].y, gt[i].theta, best_particle.x, best_particle.y, best_particle.theta);
 
-		for (int j = 0; j < 3; ++j) {
+		for (int j = 0; j < 3; ++j)
+		{
 			total_error[j] += avg_error[j];
 			cum_mean_error[j] = total_error[j] / (double)(i + 1);
 		}
@@ -138,15 +150,20 @@ int main() {
 		cout << "Cumulative mean weighted error: x " << cum_mean_error[0] << " y " << cum_mean_error[1] << " yaw " << cum_mean_error[2] << endl;
 
 		// If the error is too high, say so and then exit.
-		if (i >= time_steps_before_lock_required) {
-			if (cum_mean_error[0] > max_translation_error || cum_mean_error[1] > max_translation_error || cum_mean_error[2] > max_yaw_error) {
-				if (cum_mean_error[0] > max_translation_error) {
+		if (i >= time_steps_before_lock_required)
+		{
+			if (cum_mean_error[0] > max_translation_error || cum_mean_error[1] > max_translation_error || cum_mean_error[2] > max_yaw_error)
+			{
+				if (cum_mean_error[0] > max_translation_error)
+				{
 					cout << "Your x error, " << cum_mean_error[0] << " is larger than the maximum allowable error, " << max_translation_error << endl;
 				}
-				else if (cum_mean_error[1] > max_translation_error) {
+				else if (cum_mean_error[1] > max_translation_error)
+				{
 					cout << "Your y error, " << cum_mean_error[1] << " is larger than the maximum allowable error, " << max_translation_error << endl;
 				}
-				else {
+				else
+				{
 					cout << "Your yaw error, " << cum_mean_error[2] << " is larger than the maximum allowable error, " << max_yaw_error << endl;
 				}
 				return -1;
@@ -159,14 +176,18 @@ int main() {
 	double runtime = (stop - start) / double(CLOCKS_PER_SEC);
 	cout << "Runtime (sec): " << runtime << endl;
 
-	// Print success if accuracy and runtime are sufficient (and this isn't just the starter code).
-	if (runtime < max_runtime && pf.initialized()) {
+	// Print success if accuracy and runtime are sufficient
+	// NOTE: This isn't just for the starter code
+	if (runtime < max_runtime && pf.initialized())
+	{
 		cout << "Success! Your particle filter passed!" << endl;
 	}
-	else if (!pf.initialized()) {
+	else if (!pf.initialized())
+	{
 		cout << "This is the starter code. You haven't initialized your filter." << endl;
 	}
-	else {
+	else
+	{
 		cout << "Your runtime " << runtime << " is larger than the maximum allowable runtime, " << max_runtime << endl;
 		return -1;
 	}
