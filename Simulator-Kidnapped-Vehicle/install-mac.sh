e8e986e542662cc049da70e7fa021f7248460c0f
spgitmonish <monish.sunku@walletbuddies.co>
2017-05-30 16:15:52 -0600

    Add Simulator related files

---
+++ Simulator-Kidnapped-Vehicle/install-mac.sh
@@ -0,0 +1,16 @@
+brew install openssl libuv cmake
+git clone https://github.com/uWebSockets/uWebSockets
+cd uWebSockets
+git checkout e94b6e1
+patch CMakeLists.txt < ../cmakepatch.txt
+mkdir build
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
+cd build
+#cmake ..
+cmake -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2k -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/1.0.2k/lib ..
+make
+make
+sudo make install
+cd ..
+cd ..
+sudo rm -r uWebSockets
